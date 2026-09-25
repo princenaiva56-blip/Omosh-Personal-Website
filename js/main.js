4ea853beeecb79c845ca2fa9f8950e18bd2c0c76
Kendrince <princenaiva56@gmail.com>
2026-09-25 16:58:18 +0300

    Initialize Omosh Personal Website

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector(".site-header");
+
+window.addEventListener("scroll", () => {
+    
+    if(window.scrollY > 50){
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled")
+    }
+});
